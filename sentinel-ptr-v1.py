652fd6cf4d8a73ef1b6255be6a11892c5cda5a72
ptrthreat <98005477+ptrthreat@users.noreply.github.com>
2022-02-07 14:42:16 -0800

    Update sentinel-ptr-v1.py

--- sentinel-ptr-v1.py
+++ sentinel-ptr-v1.py
@@ -1,8 +1,10 @@
-# The purpose of this PTR Response script is to post data from PTR to sentinel
+# The purpose of this sample / example PTR Response script is to post data from PTR to sentinel
 # Step 1: Configure sentinel and PTR Setting values in Edit Configuration section
 # Step 2: Upload script as a Response Script
 # Step 3: Create a Match Condition with the Responses --> Run a script
 # Step 4: Select uploaded script
+
+# Disclaimer: Please note that this is a sample example script and is not officially supported by Proofpoint.
 # Proofpoint / Threat Response / jyun@proofpoint.com
 
 import os
